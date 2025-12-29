0150c0aed684f094f0dde7d564700729c502fb3f
Breno1112 <contato.brenofachini@hotmail.com>
2025-12-29 00:42:21 -0300

    delete user endpoint

--- app/src/controllers/user_controller.rs
+++ app/src/controllers/user_controller.rs
@@ -1,5 +1,5 @@
-use actix_web::{HttpResponse, Responder, post, web};
-use mongodb::Client;
+use actix_web::{HttpResponse, Responder, delete, post, web};
+use mongodb::{Client, bson::doc};
 
 use crate::{domain::{entities::mongodb::user_entity::UserEntity, objects::request::user::CreateUserRequest}, mappers::user_mapper::{from_create_user_request_to_user_entity, from_user_entity_to_create_user_response}};
 
@@ -11,8 +11,27 @@ async fn create_user(
     let user_entity = from_create_user_request_to_user_entity(payload.into_inner());
     let collection = mongo_client.database("fachinis").collection::<UserEntity>("users");
     match collection.insert_one(&user_entity).await {
-        Ok(result) => {
+        Ok(_) => {
             HttpResponse::Created().json(from_user_entity_to_create_user_response(user_entity))
+        }
+        Err(e) => {
+            println!("Error when inserting user. Error is {}", e.to_string());
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[delete("/{user_id}")]
+async fn delete_user(
+    mongo_client: web::Data<Client>,
+    path: web::Path<String>
+) -> impl Responder {
+    let collection = mongo_client.database("fachinis").collection::<UserEntity>("users");
+    let id = path.into_inner();
+    let filter = doc! {"_id": id};
+    match collection.delete_one(filter).await {
+        Ok(_) => {
+            HttpResponse::Ok().body("OK")
         }
         Err(e) => {
             println!("Error when inserting user. Error is {}", e.to_string());
@@ -25,5 +44,6 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/user")
         .service(create_user)
+        .service(delete_user)
     );
 }
